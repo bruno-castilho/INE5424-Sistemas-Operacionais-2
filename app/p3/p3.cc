3dc0ed4bbb21f25a79c493e6ec9b2f8a1d3e5562
Bruno <bruno.silva.castilho@gmail.com>
2024-10-29 22:19:49 -0300

    created new test

--- app/p3/p3.cc
+++ app/p3/p3.cc
@@ -11,13 +11,22 @@ const unsigned int iterations = 100;
 const Milisecond period_a = 100;
 const Milisecond period_b = 80;
 const Milisecond period_c = 60;
+const Milisecond period_d = 150;
+const Milisecond period_e = 180;
+const Milisecond period_f = 200;
 const Milisecond wcet_a = 50;
 const Milisecond wcet_b = 20;
 const Milisecond wcet_c = 10;
+const Milisecond wcet_d = 20;
+const Milisecond wcet_e = 10;
+const Milisecond wcet_f = 50;
 
 int func_a();
 int func_b();
 int func_c();
+int func_d();
+int func_e();
+int func_f();
 
 OStream cout;
 Chronometer chrono;
@@ -27,6 +36,9 @@ Semaphore write(1);
 Periodic_Thread * thread_a;
 Periodic_Thread * thread_b;
 Periodic_Thread * thread_c;
+Periodic_Thread * thread_d;
+Periodic_Thread * thread_e;
+Periodic_Thread * thread_f;
  
 Point<long, 2> p, p1(2131231, 123123), p2(2, 13123), p3(12312, 123123);
 
@@ -54,18 +66,20 @@ inline void exec(char c, Milisecond time = 0)
     Milisecond elapsed = chrono.read() / 1000;
 
     write.p();
+    cout << "\n" << elapsed << " " << c << "-1" << endl
+         <<  " A={i=" << thread_a->priority() << ",d=" << thread_a->criterion().deadline() / Alarm::frequency() << ",a=" << thread_a->leaderHead->available_time << ",b=" << thread_a->leaderHead->block_size << ",f="  << thread_a->leaderHead->frequency << ",c="  << thread_a->statistics().cycle_count << "}" << endl
+         <<  " B={i=" << thread_b->priority() << ",d=" << thread_b->criterion().deadline() / Alarm::frequency() << ",a=" << thread_b->leaderHead->available_time << ",b=" << thread_b->leaderHead->block_size << ",f="  << thread_b->leaderHead->frequency << ",c="  << thread_b->statistics().cycle_count << "}" << endl
+         <<  " C={i=" << thread_c->priority() << ",d=" << thread_c->criterion().deadline() / Alarm::frequency() << ",a=" << thread_c->leaderHead->available_time << ",b=" << thread_c->leaderHead->block_size << ",f="  << thread_c->leaderHead->frequency << ",c="  << thread_c->statistics().cycle_count << "}" << endl
+         <<  " D={i=" << thread_d->priority() << ",d=" << thread_d->criterion().deadline() / Alarm::frequency() << ",a=" << thread_d->leaderHead->available_time << ",b=" << thread_d->leaderHead->block_size << ",f="  << thread_d->leaderHead->frequency << ",c="  << thread_d->statistics().cycle_count << "}" << endl
+         <<  " E={i=" << thread_e->priority() << ",d=" << thread_e->criterion().deadline() / Alarm::frequency() << ",a=" << thread_e->leaderHead->available_time << ",b=" << thread_e->leaderHead->block_size << ",f="  << thread_e->leaderHead->frequency << ",c="  << thread_e->statistics().cycle_count << "}" << endl
+         <<  " F={i=" << thread_f->priority() << ",d=" << thread_f->criterion().deadline() / Alarm::frequency() << ",a=" << thread_f->leaderHead->available_time << ",b=" << thread_f->leaderHead->block_size << ",f="  << thread_f->leaderHead->frequency << ",c="  << thread_f->statistics().cycle_count << "}" << endl;
+
+
     cout <<  "\nCPU={id="<<  CPU::id()
          << ",curf=" <<  CPU::clock() 
          << ",minf=" <<  CPU::min_clock() 
          << ",maxf=" << CPU::max_clock() 
          << "}" << endl;
-
-
-
-    cout << "\n" << elapsed << " " << c << "-1"
-         << " [A={i=" << thread_a->priority() << ",d=" << thread_a->criterion().deadline() / Alarm::frequency() << ",a=" << thread_a->leaderHead->available_time << ",b=" << thread_a->leaderHead->block_size << ",f="  << thread_a->leaderHead->frequency << "}"
-         <<  " B={i=" << thread_b->priority() << ",d=" << thread_b->criterion().deadline() / Alarm::frequency() << ",a=" << thread_b->leaderHead->available_time << ",b=" << thread_b->leaderHead->block_size << ",f="  << thread_b->leaderHead->frequency << "}"
-         <<  " C={i=" << thread_c->priority() << ",d=" << thread_c->criterion().deadline() / Alarm::frequency() << ",a=" << thread_c->leaderHead->available_time << ",b=" << thread_c->leaderHead->block_size << ",f="  << thread_c->leaderHead->frequency << "}]";
 
 
     write.v();
@@ -77,10 +91,13 @@ inline void exec(char c, Milisecond time = 0)
 
     write.p();
     elapsed = chrono.read() / 1000;
-    cout << "\n" << elapsed << " " << c << "-2"
-         << " [A={i=" << thread_a->priority() << ",d=" << thread_a->criterion().deadline() / Alarm::frequency() << ",a=" << thread_a->leaderHead->available_time << ",b=" << thread_a->leaderHead->block_size << ",f="  << thread_a->leaderHead->frequency << "}"
-         <<  " B={i=" << thread_b->priority() << ",d=" << thread_b->criterion().deadline() / Alarm::frequency() << ",a=" << thread_b->leaderHead->available_time << ",b=" << thread_b->leaderHead->block_size << ",f="  << thread_b->leaderHead->frequency << "}"
-         <<  " C={i=" << thread_c->priority() << ",d=" << thread_c->criterion().deadline() / Alarm::frequency() << ",a=" << thread_c->leaderHead->available_time << ",b=" << thread_c->leaderHead->block_size << ",f="  << thread_c->leaderHead->frequency << "}]";
+    cout << "\n" << elapsed << " " << c << "-2" << endl
+         <<  " A={i=" << thread_a->priority() << ",d=" << thread_a->criterion().deadline() / Alarm::frequency() << ",a=" << thread_a->leaderHead->available_time << ",b=" << thread_a->leaderHead->block_size << ",f="  << thread_a->leaderHead->frequency << ",c="  << thread_a->statistics().cycle_count << "}" << endl
+         <<  " B={i=" << thread_b->priority() << ",d=" << thread_b->criterion().deadline() / Alarm::frequency() << ",a=" << thread_b->leaderHead->available_time << ",b=" << thread_b->leaderHead->block_size << ",f="  << thread_b->leaderHead->frequency << ",c="  << thread_b->statistics().cycle_count << "}" << endl
+         <<  " C={i=" << thread_c->priority() << ",d=" << thread_c->criterion().deadline() / Alarm::frequency() << ",a=" << thread_c->leaderHead->available_time << ",b=" << thread_c->leaderHead->block_size << ",f="  << thread_c->leaderHead->frequency << ",c="  << thread_c->statistics().cycle_count << "}" << endl
+         <<  " D={i=" << thread_d->priority() << ",d=" << thread_d->criterion().deadline() / Alarm::frequency() << ",a=" << thread_d->leaderHead->available_time << ",b=" << thread_d->leaderHead->block_size << ",f="  << thread_d->leaderHead->frequency << ",c="  << thread_d->statistics().cycle_count << "}" << endl
+         <<  " E={i=" << thread_e->priority() << ",d=" << thread_e->criterion().deadline() / Alarm::frequency() << ",a=" << thread_e->leaderHead->available_time << ",b=" << thread_e->leaderHead->block_size << ",f="  << thread_e->leaderHead->frequency << ",c="  << thread_e->statistics().cycle_count << "}" << endl
+         <<  " F={i=" << thread_f->priority() << ",d=" << thread_f->criterion().deadline() / Alarm::frequency() << ",a=" << thread_f->leaderHead->available_time << ",b=" << thread_f->leaderHead->block_size << ",f="  << thread_f->leaderHead->frequency << ",c="  << thread_f->statistics().cycle_count << "}" << endl;
     
     write.v();
 }
@@ -107,6 +124,9 @@ int main()
     thread_a = new Periodic_Thread(RTConf(period_a * 1000, period_a * 1000, wcet_a * 1000, 0, iterations), &func_a);
     thread_b = new Periodic_Thread(RTConf(period_b * 1000, period_b * 1000, wcet_b * 1000, 0, iterations), &func_b);
     thread_c = new Periodic_Thread(RTConf(period_c * 1000, period_c * 1000, wcet_c * 1000, 0, iterations), &func_c);
+    thread_d = new Periodic_Thread(RTConf(period_d * 1000, period_d * 1000, wcet_d * 1000, 0, iterations), &func_d);
+    thread_e = new Periodic_Thread(RTConf(period_e * 1000, period_e * 1000, wcet_e * 1000, 0, iterations), &func_e);
+    thread_f = new Periodic_Thread(RTConf(period_f * 1000, period_f * 1000, wcet_f * 1000, 0, iterations), &func_f);
 
     exec('M');
 
@@ -116,6 +136,9 @@ int main()
     int status_a = thread_a->join();
     int status_b = thread_b->join();
     int status_c = thread_c->join();
+    int status_d = thread_d->join();
+    int status_e = thread_e->join();
+    int status_f = thread_f->join();
 
     chrono.stop();
 
@@ -124,7 +147,11 @@ int main()
     cout << "\n... done!" << endl;
     cout << "\n\nThread A exited with status \"" << char(status_a)
          << "\", thread B exited with status \"" << char(status_b)
-         << "\" and thread C exited with status \"" << char(status_c) << "." << endl;
+         << "\" and thread C exited with status \"" << char(status_c) 
+         << "\" and thread D exited with status \"" << char(status_d) 
+         << "\" and thread E exited with status \"" << char(status_e) 
+         << "\" and thread F exited with status \"" << char(status_f) 
+         << "." << endl;
 
     cout << "\nThe estimated time to run the test was "
          << Math::max(period_a, period_b, period_c) * iterations
@@ -172,4 +199,43 @@ int func_c()
     exec('C');
 
     return 'C';
+}
+
+int func_d()
+{
+    exec('D');
+
+    do {
+        exec('d', wcet_d);
+    } while (Periodic_Thread::wait_next());
+
+    exec('D');
+
+    return 'D';
+}
+
+int func_e()
+{
+    exec('E');
+
+    do {
+        exec('e', wcet_e);
+    } while (Periodic_Thread::wait_next());
+
+    exec('E');
+
+    return 'E';
+}
+
+int func_f()
+{
+    exec('F');
+
+    do {
+        exec('f', wcet_f);
+    } while (Periodic_Thread::wait_next());
+
+    exec('F');
+
+    return 'F';
 }
