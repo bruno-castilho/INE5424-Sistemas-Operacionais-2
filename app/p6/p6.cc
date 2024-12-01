0284ae0c79a1e2f37fd62e7e5c9ad7dea913650c
gabriel-turatti <gabrielturattia@gmail.com>
2024-12-01 18:39:58 -0300

    Better tests

--- app/p6/p6.cc
+++ app/p6/p6.cc
@@ -189,12 +189,105 @@ int main()
     return 0;
 }
 
+
+
+
+
+void merge(int arr[], int l, int m, int r) {
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    // Create temp arrays
+    int L[n1], R[n2];
+
+    // Copy data to temp arrays L[] and R[]
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    // Merge the temp arrays back into arr[l..r
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of L[],
+    // if there are any
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of R[],
+    // if there are any
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+
+        // Sort first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
+    }
+}
+
+int binarySearch(int arr[], int low, int high, int x) {
+    if (high >= low) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == x)
+            return mid;
+
+        if (arr[mid] > x)
+            return binarySearch(arr, low, mid - 1, x);
+
+        return binarySearch(arr, mid + 1, high, x);
+    }
+
+    return -1;
+}
+
+
+
+
 int func_a()
 {
     exec('A');
 
     do {
         exec('a', wcet_a);
+
+        int* array = new int[2000];
+        array[0] = 3;
+        for (int i = 1; i < 2000; ++i) {
+            array[i] = ((array[i-1] * i * 39) % 27835);
+        }
+
+        mergeSort(array, 0, 1999);
+        binarySearch(array, 0, 1999, 371);
+
+        delete[] array;
 
     } while (Periodic_Thread::wait_next());
     exec('A');
@@ -208,6 +301,17 @@ int func_b()
 
     do {
         exec('b', wcet_b);
+
+        int* array = new int[2500];
+        array[0] = 8;
+        for (int i = 1; i < 2499; ++i) {
+            array[i] = ((array[i-1] * i * 48) % 45637);
+        }
+
+        mergeSort(array, 0, 2499);
+        binarySearch(array, 0, 2499, 835);
+
+        delete[] array;
     } while (Periodic_Thread::wait_next());
 
     exec('B');
@@ -221,6 +325,17 @@ int func_c()
 
     do {
         exec('c', wcet_c);
+
+        int* array = new int[800];
+        array[0] = 5;
+        for (int i = 1; i < 799; ++i) {
+            array[i] = ((array[i-1] * i * 75) % 45632);
+        }
+
+        mergeSort(array, 0, 799);
+        binarySearch(array, 0, 799, 391);
+
+        delete[] array;
     } while (Periodic_Thread::wait_next());
 
     exec('C');
@@ -234,6 +349,17 @@ int func_d()
 
     do {
         exec('d', wcet_d);
+
+        int* array = new int[1400];
+        array[0] = 3;
+        for (int i = 1; i < 1399; ++i) {
+            array[i] = ((array[i-1] * i * 45) % 74576);
+        }
+
+        mergeSort(array, 0, 1399);
+        binarySearch(array, 0, 1399, 795);
+
+        delete[] array;
     } while (Periodic_Thread::wait_next());
 
     exec('D');
@@ -247,6 +373,17 @@ int func_e()
 
     do {
         exec('e', wcet_e);
+
+        int* array = new int[3400];
+        array[0] = 3;
+        for (int i = 1; i < 3399; ++i) {
+            array[i] = ((array[i-1] * i * 23) % 82454);
+        }
+
+        mergeSort(array, 0, 3399);
+        binarySearch(array, 0, 3399, 543);
+
+        delete[] array;
     } while (Periodic_Thread::wait_next());
 
     exec('E');
@@ -254,12 +391,24 @@ int func_e()
     return 'E';
 }
 
+
 int func_f()
 {
     exec('F');
 
     do {
         exec('f', wcet_f);
+
+        int* array = new int[1000];
+        array[0] = 3;
+        for (int i = 1; i < 1000; ++i) {
+            array[i] = ((array[i-1] * i * 17) % 3264);
+        }
+
+        mergeSort(array, 0, 999);
+        binarySearch(array, 0, 999, 371);
+
+        delete[] array;
     } while (Periodic_Thread::wait_next());
 
     exec('F');
